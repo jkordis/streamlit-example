8ea949dd377f47f9e661509dd0f85e76c7741f76
John Kordis <104942170+jkordis@users.noreply.github.com>
2023-03-19 19:50:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,40 @@
 import streamlit as st
-import pyshark
 import pandas as pd
-import requests
-from ipaddress import ip_address, ip_network
+from scapy.all import *
 
-def process_pcap(pcap_file):
-    capture = pyshark.FileCapture(pcap_file)
-    ip_list = []
-    for pkt in capture:
+def pcap_to_dataframe(pcap_file):
+    packets = rdpcap(pcap_file)
+    data = []
+    for packet in packets:
         try:
-            src_ip = pkt.ip.src
-            dst_ip = pkt.ip.dst
-            src_public = False
-            dst_public = False
-            if ip_address(src_ip).is_private:
-                src_public = False
-            else:
-                src_public = True
-            if ip_address(dst_ip).is_private:
-                dst_public = False
-            else:
-                dst_public = True
-            ip_list.append((src_ip, dst_ip, src_public, dst_public))
-        except AttributeError:
+            src = packet[IP].src
+            dst = packet[IP].dst
+            sport = packet[IP].sport
+            dport = packet[IP].dport
+            data.append([src, dst, sport, dport])
+        except:
             pass
-    capture.close()
-    df = pd.DataFrame(ip_list, columns=["Source IP", "Destination IP", "Source Public", "Destination Public"])
-    df = df.drop_duplicates()
+    df = pd.DataFrame(data, columns=['Source IP', 'Destination IP', 'Source Port', 'Destination Port'])
     return df
 
-def virustotal_lookup(ip_address, api_key):
-    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip_address}"
-    headers = {"x-apikey": api_key}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    return data
-
 def main():
-    st.title("IP Address Extractor and Virustotal Lookup")
-    st.write("This app extracts all the found IP addresses from a pcap file and sorts them by public and private IP addresses.")
-    st.write("You can then select addresses and run them against the virustotal api to get data for each.")
-    st.write("Make sure to take the pcap and turn the object into a byte stream before passing it to pyshark.FileCapture")
+    st.title("PCAP Analyzer")
+    pcap_file = st.file_uploader("Upload a pcap file", type="pcap")
 
-    # Get the VirusTotal API key from the user
-    api_key = st.text_input("Enter your VirusTotal API key")
-    
-    # Upload the pcap file
-    file = st.file_uploader("Upload pcap file", type=["pcap", "pcapng"])
-    if file is not None:
-        df = process_pcap(file)
-        st.write("## IP Addresses Found")
+    if pcap_file is not None:
+        df = pcap_to_dataframe(pcap_file)
         st.write(df)
-        
-        # Select IP addresses to lookup in Virustotal
-        selected_ips = st.multiselect("Select IP addresses to lookup in Virustotal", df["Source IP"])
-        
-        # Run Virustotal lookup for selected IPs
-        if st.button("Lookup in Virustotal"):
-            for ip in selected_ips:
-                data = virustotal_lookup(ip, api_key)
-                st.write(f"## {ip}")
-                st.write(data)
 
-if __name__ == "__main__":
+        st.sidebar.title("Filter by:")
+        ip_address = st.sidebar.selectbox("IP Address", options=['All'] + list(df['Source IP'].unique()))
+        port = st.sidebar.selectbox("Port", options=['All'] + list(df['Source Port'].unique()))
+
+        if ip_address != 'All':
+            df = df[(df['Source IP'] == ip_address) | (df['Destination IP'] == ip_address)]
+        if port != 'All':
+            df = df[(df['Source Port'] == port) | (df['Destination Port'] == port)]
+
+        st.write(df)
+
+if __name__ == '__main__':
     main()
