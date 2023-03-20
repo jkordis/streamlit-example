f03418b243bb76389cf8caee3bb0424d38adc998
John Kordis <104942170+jkordis@users.noreply.github.com>
2023-03-19 20:21:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,90 +1,80 @@
 import streamlit as st
-from scapy.all import *
 import pandas as pd
+from scapy.all import rdpcap
 import requests
-
 
 def pcap_to_dataframe(pcap_file):
     packets = rdpcap(pcap_file)
     packet_list = []
     for packet in packets:
-        try:
-            src = packet[IP].src
-            dst = packet[IP].dst
-            sport = packet[TCP].sport
-            dport = packet[TCP].dport
-            packet_list.append((src, dst, sport, dport))
-        except:
-            pass
-    df = pd.DataFrame(packet_list, columns=['Source IP', 'Destination IP', 'Source Port', 'Destination Port'])
-    df.drop_duplicates(inplace=True)
+        packet_dict = {}
+        if 'IP' in packet:
+            packet_dict['Source IP'] = packet['IP'].src
+            packet_dict['Destination IP'] = packet['IP'].dst
+        if 'TCP' in packet:
+            packet_dict['Source Port (TCP)'] = packet['TCP'].sport
+            packet_dict['Destination Port (TCP)'] = packet['TCP'].dport
+        packet_list.append(packet_dict)
+    df = pd.DataFrame(packet_list)
     return df
-
-
-def get_public_ip(ip):
-    url = f'http://ipinfo.io/{ip}/json'
-    response = requests.get(url)
-    data = response.json()
-    if 'bogon' in data:
-        return None
-    else:
-        return data['ip']
-
 
 def get_public_ips(df):
     public_ips = []
     for ip in df['Source IP'].unique():
-        public_ip = get_public_ip(ip)
-        if public_ip is not None:
-            public_ips.append(public_ip)
-    public_ip_df = pd.DataFrame(public_ips, columns=['Public IPs'])
-    return public_ip_df
+        try:
+            response = requests.get(f'https://ipinfo.io/{ip}/json')
+            data = response.json()
+            if 'bogon' in data:
+                continue
+            if 'private' in data['ip']:
+                continue
+            public_ips.append(ip)
+        except:
+            continue
+    return public_ips
 
+def get_virustotal_results(api_key, ip):
+    headers = {'x-apikey': api_key}
+    params = {'ip': ip}
+    response = requests.get('https://www.virustotal.com/api/v3/ip_addresses',
+                            headers=headers,
+                            params=params)
+    data = response.json()
+    return data
 
 def main():
-    st.title("PCAP Analysis Tool")
-    st.sidebar.title("Settings")
-    
-    st.sidebar.subheader("Upload PCAP file")
-    pcap_file = st.sidebar.file_uploader("Choose a file")
-    
-    st.sidebar.subheader("Sort table by")
-    sort_by = st.sidebar.selectbox("", ["None", "Source IP", "Destination IP", "Source Port", "Destination Port"])
-    
-    st.sidebar.subheader("VirusTotal API key")
-    vt_api_key = st.sidebar.text_input("Enter your VirusTotal API key", type="password")
-    
-    if pcap_file is not None:
-        with st.spinner("Loading PCAP file..."):
-            df = pcap_to_dataframe(pcap_file)
-            public_ip_df = get_public_ips(df)
-        
-        if sort_by != "None":
-            df = df.sort_values(by=[sort_by])
-        
-        st.write("RAW Data Table")
-        st.write(df)
-        
-        st.write("Public IP Addresses Table")
-        public_ip_df.drop_duplicates(inplace=True)
-        public_ip_df.reset_index(drop=True, inplace=True)
-        public_ip_df['Select'] = [False] * len(public_ip_df)
-        public_ip_df = public_ip_df[['Select', 'Public IPs']]
-        public_ip_df = public_ip_df[public_ip_df['Public IPs'].str.contains('\.')].reset_index(drop=True)
-        public_ip_df = public_ip_df.groupby(['Public IPs']).first().reset_index()
-        if public_ip_df.empty:
-            st.write("No public IP addresses found.")
-        else:
-            selected_ips = st.multiselect("Select public IPs for VirusTotal scan", public_ip_df['Public IPs'])
-            if st.button("Scan selected public IPs with VirusTotal") and vt_api_key != "":
-                for ip in selected_ips:
-                    url = f'https://www.virustotal.com/vtapi/v2/ip-address/report?apikey={vt_api_key}&ip={ip}'
-                    response = requests.get(url)
-                    data = response.json()
-                    st.write(f"Results for {ip}:")
-                    st.write(data)
-    else:
-        st.warning("Please upload a PCAP file.")
-    
-if __name__ == '__main__':
+    st.title("PCAP Viewer")
+
+    st.sidebar.header("Options")
+    pcap_file = st.sidebar.file_uploader("Upload a PCAP file")
+    if not pcap_file:
+        st.sidebar.info("Please upload a PCAP file")
+        st.stop()
+    sort_by = st.sidebar.selectbox("Sort by", ["Source Port (TCP)", "Destination Port (TCP)"])
+
+    st.sidebar.subheader("Public IP Addresses")
+    df = pcap_to_dataframe(pcap_file)
+    public_ips = get_public_ips(df)
+    selected_ips = st.sidebar.multiselect("Select IP addresses to scan with VirusTotal", public_ips)
+
+    api_key = st.sidebar.text_input("Enter your VirusTotal API key")
+
+    st.sidebar.subheader("Actions")
+    if st.sidebar.button("Scan selected IPs with VirusTotal"):
+        for ip in selected_ips:
+            data = get_virustotal_results(api_key, ip)
+            st.write(f"Results for {ip}:")
+            st.write(data)
+            st.write("---")
+
+    st.subheader("RAW data")
+    st.write(df)
+    st.subheader("Sorted data")
+    if sort_by == "Source Port (TCP)":
+        sorted_df = df.sort_values("Source Port (TCP)")
+    elif sort_by == "Destination Port (TCP)":
+        sorted_df = df.sort_values("Destination Port (TCP)")
+    st.write(sorted_df)
+
+if __name__ == "__main__":
     main()
